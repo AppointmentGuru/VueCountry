5298f25e1795a9eea375bed75988daa0eaa8546a
toast38coza <info@38.co.za>
2017-01-17 08:55:26 +0200

    Apply Element.io UI lib

---
+++ src/components/SideBar.vue
@@ -0,0 +1,11 @@
+<template>
+<el-menu default-active="2" class="el-menu-vertical-demo" theme="dark">
+  <el-menu-item-group title="Getting started">
+    <el-menu-item index="1">Installation</el-menu-item>
+    <el-menu-item index="2">Usage</el-menu-item>
+  </el-menu-item-group>
+  <el-menu-item-group title="Demos">
+  </el-menu-item-group>
+  <el-menu-item index="3" >Raw data</el-menu-item>
+</el-menu>
+</template>
